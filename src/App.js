b906ad2f4054e26354a312249414eb0943f14454
Edison Enriquez <edison.enriquez@correounivalle.edu.co>
2023-05-18 21:10:28 -0500

    Project first index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Token } from '@uniswap/sdk-core';
+import { ethers, providers, Wallet } from 'ethers';
+import { POSClient,use } from '@maticnetwork/maticjs'
+import { Web3ClientPlugin } from '@maticnetwork/maticjs-ethers'
+
+// install ethers plugin
+use(Web3ClientPlugin)
+
+// Los valores son solo un ejemplo
+const chainId = 80001; // Mumbai Polygon's network ID
+// URL del punto final de la red Mumbai de Polygon
+const mumbaiRpcUrl = 'https://rpc-mumbai.maticvigil.com/';
+
+
+
+// Decimales del token
+const tokenDecimals = 0;
+const setup = async () => {
+
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+  const posClient = new POSClient();
+  
+  // Crear un proveedor JsonRpcProvider para la red Mumbai de Polygon
+  //const provider = new ethers.providers.JsonRpcProvider('https://rpc-mumbai.matic.today/');
+
+  console.log('Conectando a la red Mumbai de Polygon');
+  const PVU = new Token(
+    chainId, 
+    '0x31471e0791fcdbe82fbf4c44943255e923f1b794', 
+    tokenDecimals,
+    'PVU',
+    'PVUnist'
+  );
+  
+  await window.ethereum.request({ method: 'eth_requestAccounts' });
+  //const parentProvider = new providers.JsonRpcProvider(rpc.parent);
+  
+  console.log('Conectando a la red Mumbai de Polygon');
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const {loading, setLoading} = useState(true);
+  
+  useEffect(() => {
+    setup();
+  }, []);
+
+  return <> {loading ? 'Cargando...': <p> Hola mundo</p>} </>;
 }
 
 export default App;
